5c7bd38365740be7ff02e9e75aeff08c0010e415
Daniel Miranda <danielkza2@gmail.com>
2015-07-01 17:18:02 -0300

    Implemented entity_name for Base using a decorator
    Also move Configuration before Base to make sure it exists when used

--- kalibro_client_py/base.py
+++ kalibro_client_py/base.py
@@ -13,17 +13,46 @@ class Configuration(namedtuple('Configuration', 'host port')):
 
 
 class Base(object):
-    configuration = Configuration(None, None)
-
     def __init__(self, attributes={}):
         for name in attributes:
-                try:
-                    getattr(self, name) # Checks if the attribute is valid
-                    setattr(self, name, attributes[name])
-                except AttributeError:
-                    pass # Just ignore an invalid attribute
+            try:
+                getattr(self, name) # Checks if the attribute is valid
+                setattr(self, name, attributes[name])
+            except AttributeError:
+                pass # Just ignore an invalid attribute
 
-    def request(action, params, method='post', prefix=''):
-        url = "/%s/%s" % (endpoint)
+    @classmethod
+    def endpoint(cls):
+        return cls.entity_name() + "s"
+
+    @classmethod
+    def entity_name(cls):
+        raise NotImplementedError
+
+    def request(self, action, params, method='post', prefix=''):
+        url = "/%s/%s" % (self.endpoint())
         response = getattr(req, method)(url, params=params)
-        response.text
+        return response.text
+
+    @staticmethod
+    def entity_name_decorator():
+        """ Assign an entity name based on the class immediately inhering from Base.
+
+        This is needed because we don't want
+        entity names to come from any class that simply inherits our classes, just the ones in our module.
+
+        For example, if you create a class Project2 that exists outside of kalibro_client and inherits from Project,
+        it's entity name should still be Project.
+        """
+
+        def class_rebuilder(cls):
+            class_name = cls.__name__
+
+            class NewClass(cls):
+                @classmethod
+                def entity_name(cls):
+                    return class_name
+
+            return NewClass
+
+        return class_rebuilder
