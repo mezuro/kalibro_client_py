6fcb26c396233c2c5d8265b20530c4e4948e5b82
Daniel Miranda <danielkza2@gmail.com>
2015-07-03 14:28:36 -0300

    Fixed style issues in config. test, and added one test

--- tests/test_config.py
+++ tests/test_config.py
@@ -11,7 +11,6 @@ class TestConfig(object):
     def test_config(self):
         assert_equal(kalibro_client.config(), kalibro_client.DEFAULT_CONFIG)
 
-
     ##############################
     # kalibro_client.configure() #
     ##############################
@@ -20,20 +19,27 @@ class TestConfig(object):
     # So if it breaks you should firt look if its unit test is ok
 
     def test_configure_just_processor_address(self):
-       processor_address = 'test_proc'
+        processor_address = 'test_proc'
 
-       kalibro_client.configure(processor_address=processor_address)
+        kalibro_client.configure(processor_address=processor_address)
 
-       assert_equal(kalibro_client.config()['processor_address'], processor_address)
+        assert_equal(kalibro_client.config()['processor_address'],
+                     processor_address)
 
     def test_configure_just_configurations_address(self):
-       configurations_address = 'test_config'
+        configurations_address = 'test_config'
 
-       kalibro_client.configure(configurations_address=configurations_address)
+        kalibro_client.configure(configurations_address=configurations_address)
 
-       assert_equal(kalibro_client.config()['configurations_address'], configurations_address)
+        assert_equal(kalibro_client.config()['configurations_address'],
+                     configurations_address)
 
     def test_configure_with_no_params(self):
+        kalibro_client.configure()
+
+        assert_equal(kalibro_client.config(), kalibro_client.DEFAULT_CONFIG)
+
+    def test_configure_with_extra_params(self):
         kalibro_client.configure(test='test')
 
         assert_equal(kalibro_client.config(), kalibro_client.DEFAULT_CONFIG)
@@ -42,11 +48,13 @@ class TestConfig(object):
         processor_address = 'test_proc'
         configurations_address = 'test_config'
 
-        kalibro_client.configure(processor_address=processor_address, configurations_address=configurations_address)
+        kalibro_client.configure(processor_address=processor_address,
+                                 configurations_address=configurations_address)
 
-        assert_equal(kalibro_client.config(), {'processor_address': processor_address,
-                                               'configurations_address': configurations_address})
+        assert_equal(kalibro_client.config(),
+                     {'processor_address': processor_address,
+                     'configurations_address': configurations_address})
 
     # Ensures that modifications to _config are restored
     def tearDown(self):
-      reload(kalibro_client)
+        reload(kalibro_client)
