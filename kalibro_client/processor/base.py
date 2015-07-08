0b6a466c61f9b603acdfeaf59e0d2e4843069ac2
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-07-08 14:50:02 -0300

    Get processor address from the right configuration key

--- kalibro_client/processor/base.py
+++ kalibro_client/processor/base.py
@@ -4,4 +4,4 @@ from kalibro_client.base import Base as ClientBase
 class Base(ClientBase):
     @classmethod
     def service_address(cls):
-        return kalibro_client.config()['kalibro_processor']
+        return kalibro_client.config()['processor_address']
