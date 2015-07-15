51568ccc043178773431c7feb23c77d1b6c0524e
Daniel Miranda <danielkza2@gmail.com>
2015-07-15 11:57:08 -0300

    Implemented helpers for database cleaning

--- kalibro_client/__init__.py
+++ kalibro_client/__init__.py
@@ -1,12 +1,14 @@
-# This tells Python that just configure and config should be visible
-__all__ = ('configure', 'config')
+import requests
+
 
 DEFAULT_CONFIG = {'processor_address': "http://localhost:8082",
                   'configurations_address': "http://localhost:8083"}
 
 _config = DEFAULT_CONFIG.copy()
 
+
 # TODO: raise error if a not supported options is given
+# TODO: read from YAML
 def configure(processor_address=None, configurations_address=None, **opts):
     if processor_address:
         _config['processor_address'] = processor_address
@@ -14,8 +16,19 @@ def configure(processor_address=None, configurations_address=None, **opts):
     if configurations_address:
         _config['configurations_address'] = configurations_address
 
+
 def config():
     return _config.copy()
 
-# TODO: read from YAML
 
+def _clean_database(base_url):
+    response = requests.post(base_url + '/tests/clean_database')
+    response.raise_for_status()
+
+
+def clean_processor():
+    _clean_database(_config['processor_address'])
+
+
+def clean_configurations():
+    _clean_database(_config['configurations_address'])
