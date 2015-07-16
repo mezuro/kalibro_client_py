dea87de5c0b1006d65a7cc84cde58fdad46fc75f
Pedro Scocco <pedroscocco@gmail.com>
2015-07-16 11:40:49 -0300

    Fixed recordtype name for the classes KalibroConfiguration and MetricConfiguration
    Signed off by: Rafael Manzo <rrmanzo@gmail.com>

--- kalibro_client/configurations/kalibro_configuration.py
+++ kalibro_client/configurations/kalibro_configuration.py
@@ -3,7 +3,7 @@ from kalibro_client.configurations.base import Base
 from kalibro_client.configurations.metric_configuration import MetricConfiguration
 
 @entity_name_decorator
-class KalibroConfiguration(attributes_class_constructor('KalibroConfiguration', ('name', 'description')), Base):
+class KalibroConfiguration(attributes_class_constructor('KalibroConfigurationAttr', ('name', 'description')), Base):
 
     def metric_configurations(self):
         response = self.request(':id/metric_configurations', {'id':self.id}, method='get')
--- kalibro_client/configurations/metric_configuration.py
+++ kalibro_client/configurations/metric_configuration.py
@@ -2,5 +2,5 @@ from kalibro_client.base import attributes_class_constructor, entity_name_decora
 from kalibro_client.configurations.base import Base
 
 @entity_name_decorator
-class MetricConfiguration(attributes_class_constructor('KalibroConfiguration', ('metric', 'weight', 'aggregation_form', 'reading_group_id', 'kalibro_configuration_id')), Base):
+class MetricConfiguration(attributes_class_constructor('MetricConfigurationAttr', ('metric', 'weight', 'aggregation_form', 'reading_group_id', 'kalibro_configuration_id')), Base):
     pass
