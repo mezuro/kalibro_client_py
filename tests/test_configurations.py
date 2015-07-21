b9246afff16c7c7366df963669d17b118bed0192
Diego Araújo <diegoamc90@gmail.com>
2015-07-21 12:10:07 -0300

    Implemented Reading's methods
    Signed off by: Pedro Scocco <pedroscocco@gmail.com>

--- tests/test_configurations.py
+++ tests/test_configurations.py
@@ -4,11 +4,13 @@ from kalibro_client.configurations import MetricConfiguration, Reading
 
 import kalibro_client
 
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_true
 
 from mock import patch
 from factories import KalibroConfigurationFactory, MetricConfigurationFactory, \
     ReadingGroupFactory, ReadingFactory
+
+from .helpers import not_raises
 
 class TestConfigurationsBase(TestCase):
     @patch('kalibro_client.config')
@@ -38,10 +40,40 @@ class TestReadingGroup(TestCase):
         self.reading = ReadingFactory.build()
         self.readings = [self.reading]
 
-    def test_metric_configurations(self):
+    def test_readings(self):
         readings_hash = {"readings": [self.reading._asdict()]}
         with patch.object(self.subject, 'request', return_value=readings_hash) as request_mock, \
              patch.object(Reading, 'response_to_objects_array', return_value=self.readings) as mock:
             self.subject.readings()
             request_mock.assert_called_once_with(":id/readings", {'id': 1}, method='get')
             mock.assert_called_once_with(readings_hash)
+
+class TestReading(TestCase):
+    def setUp(self):
+        self.subject = ReadingFactory.build()
+        self.readings = [self.subject]
+
+    def test_properties_getters(self):
+        assert_true(hasattr(self.subject, 'reading_group_id'))
+
+    @not_raises((AttributeError, ValueError))
+    def test_properties_setters(self):
+        self.subject.reading_group_id = None
+
+    def test_readings_of(self):
+        readings_hash = {"readings": [self.subject._asdict()]}
+        reading_group_id = 1
+        with patch.object(Reading, 'request', return_value=readings_hash) as request_mock, \
+             patch.object(Reading, 'response_to_objects_array', return_value=self.readings) as mock:
+            Reading.readings_of(reading_group_id)
+            request_mock.assert_called_once_with(action='', params={}, method='get', prefix="reading_groups/{}".format(reading_group_id))
+            mock.assert_called_once_with(readings_hash)
+
+    def test_save_prefix(self):
+        assert_equal(self.subject.save_prefix(), "reading_groups/{}".format(self.subject.reading_group_id))
+
+    def test_update_prefix(self):
+        assert_equal(self.subject.update_prefix(), "reading_groups/{}".format(self.subject.reading_group_id))
+
+    def test_delete_prefix(self):
+        assert_equal(self.subject.delete_prefix(), "reading_groups/{}".format(self.subject.reading_group_id))
