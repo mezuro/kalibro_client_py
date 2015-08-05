mezuro/kalibro_client_py

commit 246236e6a9823bbc6175e1405d8b58990108bda5

Fixed Repository's processing in time methods
datetime is not JSON serializable, so we must make the request with it converted to string

Signed off by: Pedro Scocco <pedroscocco@gmail.com>

kalibro_client/processor/repository.py
@@ -114,12 +114,12 @@ class Repository(attributes_class_constructor('RepositoryAttr',
     def has_processing_after(self, date):
         return self.request(":id/has_processing/after",
                             params={'id': self.id,
-                                    'date': date})['has_processing_in_time']
+                                    'date': str(date)})['has_processing_in_time']
 
     def has_processing_before(self, date):
         return self.request(":id/has_processing/before",
                             params={'id': self.id,
-                                    'date': date})['has_processing_in_time']
+                                    'date': str(date)})['has_processing_in_time']
 
     def last_processing_state(self):
         return self.request(":id/last_processing_state",
@@ -142,12 +142,12 @@ class Repository(attributes_class_constructor('RepositoryAttr',
     def first_processing_after(self, date):
         return Processing(**self.request(":id/first_processing/after",
                                          params={'id': self.id,
-                                                 'date': date})["processing"])
+                                                 'date': str(date)})["processing"])
 
     def last_processing_before(self, date):
         return Processing(**self.request(":id/last_processing/before",
                                          params={'id': self.id,
-                                                 'date': date})['processing'])
+                                                 'date': str(date)})['processing'])
 
     @classmethod
     def branches(cls, url, scm_type):

tests/test_processor.py
@@ -126,7 +126,7 @@ class TestKalibroModule(TestCase):
 class TestRepository(TestCase):
     def setUp(self):
         self.subject = RepositoryFactory.build()
-        self.date_str = "2015-07-05T22:16:18+00:00"
+        self.date_str = "2015-07-05 22:16:18+00:00"
         self.date = dateutil.parser.parse(self.date_str)
 
     def test_properties_getters(self):
@@ -272,7 +272,7 @@ class TestRepository(TestCase):
         with patch.object(Repository, 'request',
                           return_value=has_processing_after) as repository_request:
             response = self.subject.has_processing_after(self.date)
-            repository_request.assert_called_once_with(':id/has_processing/after', params={'id': self.subject.id, 'date': self.date})
+            repository_request.assert_called_once_with(':id/has_processing/after', params={'id': self.subject.id, 'date': self.date_str})
             assert_equal(response, True)
 
     def test_has_processing_before(self):
@@ -280,7 +280,7 @@ class TestRepository(TestCase):
         with patch.object(Repository, 'request',
                           return_value=has_processing_before) as repository_request:
             response = self.subject.has_processing_before(self.date)
-            repository_request.assert_called_once_with(':id/has_processing/before', params={'id': self.subject.id, 'date': self.date})
+            repository_request.assert_called_once_with(':id/has_processing/before', params={'id': self.subject.id, 'date': self.date_str})
             assert_equal(response, True)
 
     def test_last_processing_state(self):
@@ -327,7 +327,7 @@ class TestRepository(TestCase):
         with patch.object(Repository, 'request',
                           return_value=processing_hash) as repository_request:
             response = self.subject.first_processing_after(self.date)
-            repository_request.assert_called_once_with(':id/first_processing/after', params={'id': self.subject.id, 'date': self.date})
+            repository_request.assert_called_once_with(':id/first_processing/after', params={'id': self.subject.id, 'date': self.date_str})
             assert_equal(response, processing)
 
     def test_last_processing_before(self):
@@ -337,7 +337,7 @@ class TestRepository(TestCase):
         with patch.object(Repository, 'request',
                           return_value=processing_hash) as repository_request:
             response = self.subject.last_processing_before(self.date)
-            repository_request.assert_called_once_with(':id/last_processing/before', params={'id': self.subject.id, 'date': self.date})
+            repository_request.assert_called_once_with(':id/last_processing/before', params={'id': self.subject.id, 'date': self.date_str})
             assert_equal(response, processing)
 
     def test_branches(self):

commit 5d1615c860487993f61f732a9a7971fa453861ef

Fixed Repository processing time methods to use correct format
Using str(date) happened to work, but there's no guarantee it will be in
ISO8601 format (or any in particular), as the services require. Using
the isoformat() method instead is better.

Fixes: 246236e6a9823bbc6175e1405d8b58990108bda5

kalibro_client/processor/repository.py
@@ -114,12 +114,12 @@ class Repository(attributes_class_constructor('RepositoryAttr',
     def has_processing_after(self, date):
         return self.request(":id/has_processing/after",
                             params={'id': self.id,
-                                    'date': str(date)})['has_processing_in_time']
+                                    'date': date.isoformat()})['has_processing_in_time']
 
     def has_processing_before(self, date):
         return self.request(":id/has_processing/before",
                             params={'id': self.id,
-                                    'date': str(date)})['has_processing_in_time']
+                                    'date': date.isoformat()})['has_processing_in_time']
 
     def last_processing_state(self):
         return self.request(":id/last_processing_state",
@@ -142,12 +142,12 @@ class Repository(attributes_class_constructor('RepositoryAttr',
     def first_processing_after(self, date):
         return Processing(**self.request(":id/first_processing/after",
                                          params={'id': self.id,
-                                                 'date': str(date)})["processing"])
+                                                 'date': date.isoformat()})["processing"])
 
     def last_processing_before(self, date):
         return Processing(**self.request(":id/last_processing/before",
                                          params={'id': self.id,
-                                                 'date': str(date)})['processing'])
+                                                 'date': date.isoformat()})['processing'])
 
     @classmethod
     def branches(cls, url, scm_type):

tests/test_processor.py
@@ -126,7 +126,7 @@ class TestKalibroModule(TestCase):
 class TestRepository(TestCase):
     def setUp(self):
         self.subject = RepositoryFactory.build()
-        self.date_str = "2015-07-05 22:16:18+00:00"
+        self.date_str = "2015-07-05T22:16:18+00:00"
         self.date = dateutil.parser.parse(self.date_str)
 
     def test_properties_getters(self):