97c18003abf10b497a8f4e8b49208942ff5c76cb
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-09-03 11:08:40 -0300

    setup.py created
    Signed off by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+"""
+KalibroClient
+===================
+
+A http client for the Kalibro services.
+"""
+from setuptools import setup, find_packages
+
+install_requires = [
+    'requests==2.7.0',
+    'wheel==0.24.0',
+    'inflection==0.3.1',
+    'recordtype==1.1',
+    'python-dateutil==2.4.2',
+    'factory_boy==2.5.2',
+    'enum34>=1.0',
+]
+
+tests_require = [
+    'nose==1.3.7',
+    'nose-progressive==1.5.1',
+    'coverage==3.7.1',
+    'mock==1.0.1',
+    'behave==1.2.5',
+]
+
+
+setup(
+    name="kalibro_client",
+    version='1.3.0',
+    author='Rafael Manzo',
+    author_email='rr.manzo@gmail.com',
+    url='https://github.com/mezuro/kalibro_client_py',
+    description='A http client for the Kalibro services.',
+    long_description=__doc__,
+    license='LGPLv3',
+    package_dir={'': 'kalibro_client'},
+    packages=find_packages('kalibro_client'),
+    zip_safe=True,
+    install_requires=install_requires,
+    test_suite="nose.collector",
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development'
+    ],
+)
