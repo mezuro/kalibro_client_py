8b6cdacfb8d724d4c0d800b3a594f9ff6e95df6f
Heitor Reis <marcheing@gmail.com>
2015-10-02 16:40:52 -0300

    Refactor Statistic inheritance
     * Now Statistic inherits from RequestMethods preventing the need for
     not implemented methods inherited from BaseCRUD
    
    Signed off by: Diego Araújo <diegoamc90@gmail.com>

--- kalibro_client/configurations/statistic.py
+++ kalibro_client/configurations/statistic.py
@@ -1,30 +1,13 @@
 from kalibro_client.configurations.base import Base
-from kalibro_client.base import entity_name_decorator
+from kalibro_client.base import entity_name_decorator, RequestMethods
 
 @entity_name_decorator
-class Statistic(Base):
+class Statistic(RequestMethods):
     @classmethod
     def metric_percentage(cls, metric_code):
         return cls.request("/metric_percentage",
             {"metric_code": metric_code}, method="get")
 
     @classmethod
-    def find(cls, id):
-        raise NotImplementedError
-
-    @classmethod
-    def all(cls):
-        raise NotImplementedError
-
-    @classmethod
-    def exists(cls, id):
-        raise NotImplementedError
-
-    def save(self):
-        raise NotImplementedError
-
-    def update(self, **kwargs):
-        raise NotImplementedError
-
-    def delete(self):
-        raise NotImplementedError
+    def service_address(cls):
+        return Base.service_address()
